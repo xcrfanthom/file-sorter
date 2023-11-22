aefce685cc8ac690a22d264891374f41471680c6
Bartu_Akarsu <akarsubartu@gmail.com>
2023-11-22 21:25:37 +0300

    commit

---
+++ sort.py
@@ -0,0 +1,40 @@
+import os
+import shutil
+
+source_folder = r"C:\Users\ASUS\Downloads"
+destination_folder = r"C:\Users\ASUS\Desktop\Files"
+
+if not os.path.exists(destination_folder):
+    os.mkdir(destination_folder)
+    
+extension_folder_mapping = {
+    "txt": "Text",
+    "pdf": "PDF",
+    "jpg": "Images",
+    "png": "Images",
+    "docx": "Documents",
+    "zip": "Compressed",
+    "xlsx": "Spreadsheets",
+    "exe": "Executables",
+    "ini": "Configuration",
+    "mp3": "Audio",
+    "mp4": "Videos",
+    "py": "Python",
+    "wav": "Raw-Audio",
+    "pptx": "Presentations",
+}
+
+def sort_files_by_extension(source, destination):
+    for filename in os.listdir(source):
+        file_extension = filename.split(".")[-1].lower()
+        source_path = os.path.join(source, filename)
+        
+        if os.path.isfile(source_path):
+            if file_extension in extension_folder_mapping:
+                destination_path = os.path.join(destination, extension_folder_mapping[file_extension])
+                if not os.path.exists(destination_path):
+                    os.mkdir(destination_path)
+                shutil.move(source_path, os.path.join(destination_path, filename))
+            else:
+                print(f"Extension '{file_extension}' not in the mapping. Skipping '{filename}'.")
+sort_files_by_extension(source_folder, destination_folder)
